6e3dfb0a1ec37380a411870c66a6ee28c77a5e9d
JanoBourian <supermath33@hotmail.com>
2025-03-16 03:11:48 -0600

    add: requirements.txt and black and ruff

--- decorators_and_closures/introduction.py
+++ decorators_and_closures/introduction.py
@@ -1,11 +1,14 @@
 def deco(_):
     def inner():
         print("running inner()")
+
     return inner
+
 
 @deco
 def target():
     pass
+
 
 target()
 
@@ -13,27 +16,33 @@ target()
 
 registry = []
 
+
 def register(func):
     print(f"running register({func})")
     registry.append(func)
     return func
 
+
 @register
 def f1():
     print("running f1()")
 
+
 @register
 def f2():
     print("running f2()")
-    
+
+
 def f3():
     print("running f3()")
-    
+
+
 def main():
     print("running main()")
     print("registry ->", registry)
     f1()
     f2()
     f3()
+
 
 main()
